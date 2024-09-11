39f09963f860c9496222e8f45df50a425e9c11ca
Farahana Suhaimi <farahanams@gmail.com>
2024-09-11 10:29:15 +0800

    Refactor AuthServiceProvider to use PostPolicy instead of BlogpostPolicy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        Blogpost::class => BlogpostPolicy::class,
+        Blogpost::class => PostPolicy::class,
     ];
 
     /**
